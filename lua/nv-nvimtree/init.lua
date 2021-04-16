9f9472a288ed732dc5a857bfb677cd08bc003adf
bnew10 <bradnew10@gmail.com>
2021-04-16 16:15:54 -0700

    disabled netrw disable funtionality

--- lua/nv-nvimtree/init.lua
+++ lua/nv-nvimtree/init.lua
@@ -4,6 +4,7 @@ vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_disable_netrw = 0
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
