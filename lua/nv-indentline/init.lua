82cf88375c2b2a001f21f58fe642bcbe6c54a0ee
bnew10 <bradnew10@gmail.com>
2021-04-22 16:57:58 -0700

    added packer to blacklist and rearranged settings

--- lua/nv-indentline/init.lua
+++ lua/nv-indentline/init.lua
@@ -3,8 +3,8 @@ vim.g.indent_blankline_char = '▏'
 
 vim.cmd 'hi IndentBlanklineChar guifg=#2a2e36'
 
-vim.g.indent_blankline_filetype_exclude = {'help', 'terminal'}
+vim.g.indent_blankline_filetype_exclude = {'help', 'terminal', 'packer'}
 vim.g.indent_blankline_buftype_exclude = {'terminal'}
 
-vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = false
+vim.g.indent_blankline_show_trailing_blankline_indent = false
