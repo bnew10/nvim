d8b63f8ed282876a205faa3755557844c782bfb5
bnew10 <bradnew10@gmail.com>
2021-04-27 19:08:20 -0700

    added new symbols, keybindings, and highlighting

--- lua/nv-gitsigns/init.lua
+++ lua/nv-gitsigns/init.lua
@@ -1,10 +1,10 @@
 require('gitsigns').setup {
   signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '契', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '契', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add          = {hl = 'GitSignsAdd'   , text = '▌', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '▌', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
   },
   numhl = false,
   linehl = false,
@@ -12,6 +12,13 @@ require('gitsigns').setup {
     -- Default keymap options
     noremap = true,
     buffer = true,
+    ['n <Leader>gj'] = '<cmd>lua require"gitsigns".next_hunk()<CR>',
+    ['n <Leader>gk'] = '<cmd>lua require"gitsigns".prev_hunk()<CR>',
+    ['n <Leader>gb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+	['n <leader>gs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['n <leader>gu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <leader>gr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['n <leader>gp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
   },
   watch_index = {
     interval = 1000
@@ -19,3 +26,9 @@ require('gitsigns').setup {
   update_debounce = 200,
   use_decoration_api = false
 }
+
+local cmd = vim.cmd
+
+cmd('hi GitSignsAdd guifg='..O.green..' guibg=NONE')
+cmd('hi GitSignsChange guifg='..O.yellow..' guibg=NONE')
+cmd('hi GitSignsDelete guifg='..O.red..' guibg=NONE')
