066fb4461709cfa0a9ca23062b8a0e014ec2c607
Bradley Newton <bnewton@vcinity.io>
2021-09-07 17:59:32 -0700

    added pycodestyle error to ignore

--- lua/lsp/python-ls.lua
+++ lua/lsp/python-ls.lua
@@ -1,5 +1,15 @@
 -- pip install 'python-lsp-server[all]'
 require'lspconfig'.pylsp.setup {
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    ignore = {'E501'}
+                }
+            }
+        }
+
+    },
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
             virtual_text = false,
