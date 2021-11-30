21a0a646236d468388c2ce3107cae5556577fb3e
bnew10 <bradnew10@gmail.com>
2021-11-29 21:34:35 -0800

    changed easy lsp installer to new and active plugin

--- lua/lsp/clangd-ls.lua
+++ lua/lsp/clangd-ls.lua
@@ -1,5 +1,5 @@
 require'lspconfig'.clangd.setup {
-    cmd = {DATA_PATH .. "/lspinstall/cpp/clangd/bin/clangd"},
+    cmd = {DATA_PATH .. "/lsp_servers/clangd/clangd"},
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
             virtual_text = false,
